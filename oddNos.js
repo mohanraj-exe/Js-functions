33363cfed658f36a72834839796ae2453f52df64
mohanraj-exe <95164416+mohanraj-exe@users.noreply.github.com>
2022-02-23 20:31:13 +0530

    Update oddNos.js

--- oddNos.js
+++ oddNos.js
@@ -10,3 +10,30 @@ function oddNos(arr){
     return result
 }
 console.log(oddNos([2,3,4,5])) //[3,5]
+
+// Anonymous function
+var oddNos = function (arr){
+    var res = []
+    for(let i=0; i<arr.length; i++){
+
+        if(arr[i]%2 !==0){
+            res.push(arr[i])
+        }
+    }
+    return res;
+
+}
+console.log(oddNos([1,2,3,4,5,6]))
+
+// By arrow functions
+var func = (arr) => {
+
+    var res = []
+    for(i=0;i<arr.length; i++){
+        if(arr[i]%2 !== 0){
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+console.log(func([1,2,3,4,5,6]))
