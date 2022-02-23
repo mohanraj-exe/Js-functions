2a5bd7bd24c3253de2886d7cef0033ffc668aeba
mohanraj-exe <95164416+mohanraj-exe@users.noreply.github.com>
2022-02-23 20:34:38 +0530

    Update sumOfAll.js

--- sumOfAll.js
+++ sumOfAll.js
@@ -9,3 +9,27 @@ function sumOfAll(arr){
     return sum
 }
 console.log(sumOfAll([2,3,4,5])) // 14
+
+// By Anonymous function
+var iniz = function (arr){
+
+    var sum = 0
+    for(i=0; i<arr.length; i++){
+        
+        sum = sum + arr[i]
+    }
+    return sum
+}
+var res = iniz([1,2,3,4,5])
+console.log(res);
+
+//Arrow functions
+var res = (arr) =>{
+
+    var sum = 0;
+    for(i in arr){
+      sum += arr[i] 
+    }
+    return sum
+}
+console.log(res([1,2,3,4,5]))
