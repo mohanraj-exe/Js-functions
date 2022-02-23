0e320491877c39dd7b59893979983600ac9fd0a9
mohanraj-exe <95164416+mohanraj-exe@users.noreply.github.com>
2022-02-23 20:38:02 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -11,3 +11,33 @@ function palindrome(arr){
     return result;
 }
 console.log(palindrome([121,232,'eve','exe','dood','asdfgf'])) //[121,232,'eve','exe','dood']
+
+// Anonymous function
+var result = function palindrome(arr){
+
+    var res = []
+    for(i=0; i<arr.length; i++){
+        var str = arr[i].toString()
+        var char = str.split("").reverse().join("");
+        if(char == arr[i]){
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+console.log(result([121,232,"eve","dood","lol","hello","geek","guvi"]))
+
+// By arrow function
+var res = (arr) => {
+
+    var result = []
+    
+    for(i=0; i<arr.length; i++){
+        var reversedStr = arr[i].toString().split("").reverse().join("");
+        if(arr[i] == reversedStr){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+console.log(res([121,232, "eve", "dood", "lol","hello","guvi","geek"]))
